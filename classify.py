07ac90672c7dfb65ba92a38acb896f17062a8b7f
Kavin Yao <kavinyao@gmail.com>
2013-11-05 01:58:18 -0800

    add classification skeleton

---
+++ classify.py
@@ -0,0 +1,28 @@
+import os
+import SimpleCV as cv
+
+extractors = []
+#hhfe = cv.HueHistogramFeatureExtractor()
+#extractors.append(hhfe)
+ehfe = cv.EdgeHistogramFeatureExtractor()
+extractors.append(ehfe)
+
+svm_properties = {
+    'KernelType':'RBF', #default is a RBF Kernel
+    'SVMType':'NU',     #default is C
+    'nu':0.05,          # NU for SVM NU
+    'c':None,           #C for SVM C - the slack variable
+    'degree':None,      #degree for poly kernels - defaults to 3
+    'coef':None,        #coef for Poly/Sigmoid defaults to 0
+    'gamma':None,       #kernel param for poly/rbf/sigma - default is 1/#samples
+}
+svm = cv.SVMClassifier(extractors, svm_properties)
+
+data_dir = 'data'
+classes = os.listdir(data_dir)
+
+train_paths = [os.path.join(data_dir, c, 'train') for c in classes]
+test_paths = [os.path.join(data_dir, c, 'test') for c in classes]
+
+print svm.train(train_paths, classes, verbose=True)
+print svm.test(test_paths, classes, verbose=True)
